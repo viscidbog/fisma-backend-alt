328950e1ab61e346130f42778b061fa7d9a8842a
Janne Puukko <124368715+viscidbog@users.noreply.github.com>
2025-04-28 10:40:34 +0300

    Update SecurityConfig.java

--- src/main/java/fi/fisma/backend/security/SecurityConfig.java
+++ src/main/java/fi/fisma/backend/security/SecurityConfig.java
@@ -121,7 +121,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://benefit.pinkkhub.com", "http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("https://fisma-benefit-app.github.io", "http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setExposedHeaders(List.of("Location"));
